ac5c47eb0d47aeb646c8dd41afa079a7ff39f097
TEK Student <TEKstudent@vlab.lan>
2025-08-18 11:55:40 -0700

    Remove cloud block

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  cloud {
-    organization = "policy-as-code-training"
-    workspaces {
-      name = "tf-vault-qa-cb"
-    }
-  }
+  # cloud {
+  #   organization = "policy-as-code-training"
+  #   workspaces {
+  #     name = "tf-vault-qa-cb"
+  #   }
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
